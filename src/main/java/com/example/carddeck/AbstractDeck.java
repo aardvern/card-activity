484f2b0427a3adef2ed08d2b3d1951f9461f9050
Aardweyn <stinkycheeseman46@gmail.com>
2021-09-16 19:31:11 -0700

    Initial commit

---
+++ src/main/java/com/example/carddeck/AbstractDeck.java
@@ -0,0 +1,21 @@
+package com.example.carddeck;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+public abstract class AbstractDeck implements Deck {
+
+    protected List<Card> cards;
+
+    public List<Card> getCards() {
+        return this.cards;
+    }
+
+    @Override
+    public void shuffle() {
+        long seed = System.nanoTime();
+        Collections.shuffle(this.cards, new Random(seed));
+    }
+}
